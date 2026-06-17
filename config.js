0add278469ca3fb7eb43c7fb4f27c9dd15b688ef
mblvaa <mashkawell@gmail.com>
2026-06-17 10:52:44 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+// Ссылка на опубликованный Google Apps Script
+window.PSY_CONFIG = {
+  API_URL: "https://script.google.com/macros/s/AKfycbyAEwZxuNbohTKbspUv8tuNQ3oVnVU40bbCgUAqIHVO_fHMHFAOjCqDjE1_JBUQcU2P/exec"
+};
